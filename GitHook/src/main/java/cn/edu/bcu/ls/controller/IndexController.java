1fc8a7425527191fb2ae1b188e15e0fe3041c28a
vicmeng <1137998920@qq.com>
2020-05-15 22:11:48 +0800

    2

--- GitHook/src/main/java/cn/edu/bcu/ls/controller/IndexController.java
+++ GitHook/src/main/java/cn/edu/bcu/ls/controller/IndexController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 	@RequestMapping("/")
 	public String index() {
-		return "hello word ls zjx 1";
+		return "hello word ls zjx 2";
 	}
 }
