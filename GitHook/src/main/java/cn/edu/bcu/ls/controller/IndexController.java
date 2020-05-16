6a4bbfb526715677dea6f173beea5ee545707bf6
vicmeng <1137998920@qq.com>
2020-05-16 11:08:23 +0800

    17

--- GitHook/src/main/java/cn/edu/bcu/ls/controller/IndexController.java
+++ GitHook/src/main/java/cn/edu/bcu/ls/controller/IndexController.java
@@ -1,6 +1,6 @@
 package cn.edu.bcu.ls.controller;
 
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 	@RequestMapping("/")
 	public String index() {
-		return "hello word ls zjx 16";
+		return "hello word ls zjx 17";
 	}
 }
